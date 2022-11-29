996c532a156abae83e2d7bcf63ed6631a7171fcc
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2022-11-29 09:32:50 -0300

    Barra de Status

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 import LifeStatus from '../../Components/Common/LifeStatus';
+import StatusBar from '../../Components/Home/StatusBar';
 
 export default function Home() {
 
@@ -20,6 +21,7 @@ export default function Home() {
                 <View style={{alignItems: "center"}}>
                     <Text style={styles.dailyChecks}> ❤️ 20 dias - ✔️ 80 checks </Text>
                     <LifeStatus />
+                    <StatusBar />
                 </View>
 
                 <Text 
